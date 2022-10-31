9cc504e39eab1a271df37cb0e26e04092680b74b
Jiří Vrba <jiri@vrba.dev>
2022-10-31 12:25:21 +0100

    Import part of mock data from the database

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react";
 import { AbsoluteFill, Sequence } from "remotion";
+import { Score } from "./types";
 
 export interface ResultsProps {
-
+  results: Array<Score>
 }
 
-export const Results: FC<ResultsProps> = ({ }: ResultsProps) => {
+export const Results: FC<ResultsProps> = ({ results }: ResultsProps) => {
   return (
     <AbsoluteFill style={{ background: "#222222" }}>
       <Sequence from={0}>
         <h1>Good Gartic's Halloween</h1>
+        {JSON.stringify(results)}
       </Sequence>
     </AbsoluteFill>
   )
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface Score {
+  avatar: string,
+  username: string,
+  points: number,
+  items: Array<string>
+}
