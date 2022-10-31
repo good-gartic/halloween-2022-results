2870005980b40a59d8ef9e87d5671bc81e83d380
Jiří Vrba <jiri@vrba.dev>
2022-10-31 15:34:56 +0100

    Improved visuals + added music

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
-import { AbsoluteFill, Img, interpolate, random, useCurrentFrame, useVideoConfig } from "remotion";
+import { CSSProperties, FC } from "react";
+import { AbsoluteFill, Easing, Img, interpolate, random, useCurrentFrame, useVideoConfig } from "remotion";
 import { Score } from "./types";
+import styled from "@emotion/styled";
 
 export interface ResultProps {
   place: number,
@@ -12,61 +13,81 @@ const Items: FC<{ seed: string, items: Array<string> }> = ({ seed, items }) => {
   const frame = useCurrentFrame();
 
   return (
-    <AbsoluteFill>
-      <div style={{ fontSize: 30, color: "#ffffff" }}>{frame}</div>
+    <AbsoluteFill style={{ opacity: 0.25 }}>
       {
         items.map((emoji, i) => {
           const base = `${seed}${i}`;
           const size = random(base + "size") * 30 + 20;
-          const offset = ((random(seed + i.toString() + "y") * height) + (frame * size * 0.25) * 0.25) % height;
-          const opacity = interpolate(offset, [0, height / 4, height / 2, height / 4 * 3, height], [0, 0.1, 0.5, 0.1, 0]);
-          const style = css`
-              position: absolute;
-              fontSize: ${size}px;
-              opacity: ${opacity};
-              transform: rotate(${random(base + "rotate") * 20 - 10}deg);
-              left: ${random(base + "x") * width}px;
-              top: ${offset}px;
-          `;
 
-          return (
-            <div key={i}
-              style={{
-                position: "absolute",
-                fontSize: size + "px",
-                opacity: opacity,
-                transform: `rotate(${random(base + "rotate") * 20 - 10}deg)`,
-                left: (random(base + "x") * width) + "px",
-                top: offset + "px"
-              }}
-            >{emoji}</div>
-          );
+          const left = (random(seed + i.toString() + "y") * width * 1.2) - width * 0.2 + ((frame / 150) * width * 0.2) * (size / 50);
+          const top = (random(base + "x") * height);
+
+          const style: CSSProperties = {
+            left: left + "px",
+            top: top + "px",
+            position: "absolute",
+            fontSize: size + "px",
+            transform: `rotate(${random(base + "rotate") * 20 - 10}deg)`,
+            zIndex: Math.floor(size),
+          };
+
+          return <div key={i} style={style}>{emoji}</div>;
         })
       }
     </AbsoluteFill>
   )
 };
 
+const Place = styled.div`
+  font-family: 'JetBrains Mono';
+  font-size: 60px;
+  font-weight: bold;
+  color: #ffaa00;
+`;
+
+const Points = styled.div`
+  font-family: 'JetBrains Mono';
+  font-size: 100px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const Member = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+`;
+
+const Username = styled.div`
+  color: #ffffff;
+  font-size: 60px;
+  font-weight: bold;
+  font-family: 'JetBrains Mono';
+  margin-left: 50px;
+`;
+
 const User: FC<{ score: Score, place: number }> = ({ score, place }) => {
   const frame = useCurrentFrame();
-  const points = interpolate(frame, [0, 25], [0, score.points], { extrapolateRight: "clamp" });
+  const points = interpolate(frame, [0, 10, 80], [0, 0, score.points], { extrapolateRight: "clamp", easing: Easing.out(Easing.ease) });
 
   return (
     <AbsoluteFill style={{ display: "flex", flexFlow: "column nowrap", justifyContent: "center", alignItems: "center" }}>
-      <div style={{ fontSize: 30, color: "#ffaa00" }}>#{place}</div>
-      <div style={{ fontSize: 50, color: "#ffffff" }}>{Math.floor(points)} points</div>
+      <Place>#{place}</Place>
+      <Points>{Math.floor(points)} points</Points>
 
-      <div style={{ display: "flex", flexFlow: "row nowrap", justifyContent: "center", alignItems: "center", marginTop: "30px" }}>
+      <Member>
         <Img src={score.avatar} style={{ width: 150, height: 150, borderRadius: "50%" }}></Img>
-        <div style={{ fontSize: 50, color: "#ffffff", marginLeft: 30 }}>{score.username}</div>
-      </div>
+        <Username>{score.username}</Username>
+      </Member>
     </AbsoluteFill>
   )
 };
 
 export const Result: FC<ResultProps> = ({ place, score }: ResultProps) => {
   const frame = useCurrentFrame();
-  const opacity = interpolate(frame, [0, 10, 90, 100], [0, 1, 1, 0]);
+  const opacity = interpolate(frame, [0, 10, 90, 100, 150], [0, 1, 1, 1, 0]);
 
   return (
     <AbsoluteFill style={{ opacity: opacity }}>
--- src/Results.tsx
+++ src/Results.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
-import { AbsoluteFill, Sequence, Series } from "remotion";
+import { AbsoluteFill, Audio, Sequence, Series } from "remotion";
 import { Result } from "./Result";
 import { Score } from "./types";
+import music from "./assets/music.mp3";
 
 export interface ResultsProps {
   results: Array<Score>
@@ -11,19 +12,21 @@ export const Results: FC<ResultsProps> = ({ results }: ResultsProps) => {
   const sorted = results.sort((a, b) => a.points - b.points);
 
   return (
-    <AbsoluteFill style={{ background: "#222222" }}>
-      <Series>
-        <Series.Sequence durationInFrames={100}>
-          <h1>Good Gartic's Halloween</h1>
-        </Series.Sequence>
-        {
-          sorted.map((score, i) => (
-            <Series.Sequence key={i} durationInFrames={100}>
-              <Result score={score} place={sorted.length - i} />
-            </Series.Sequence>
-          ))
-        }
-      </Series>
-    </AbsoluteFill>
+    <>
+      <AbsoluteFill style={{ background: "#222222" }}>
+        <Sequence from={230}>
+          <Series>
+            {
+              sorted.map((score, i) => (
+                <Series.Sequence key={i} durationInFrames={150}>
+                  <Result score={score} place={sorted.length - i} />
+                </Series.Sequence>
+              ))
+            }
+          </Series>
+        </Sequence>
+      </AbsoluteFill>
+      <Audio src={music} />
+    </>
   )
 };
